58338a16b7266f7368eb4d40caeed3dd75c46fca
Myhyem <hellohj_@naver.com>
2020-06-20 21:20:41 +0900

    1. 생정자 메소드/클래스 메소드

---
+++ src/TPC31.java
@@ -0,0 +1,11 @@
+import kr.tpc.Inflearn;
+
+public class TPC31 {
+    public static void main(String[] args) {
+        /*Inflearn inf = new Inflearn();
+        inf.tpc();
+        inf.java();*/
+        Inflearn.tpc();
+        Inflearn.java();
+    }
+}
---
+++ src/kr/tpc/Inflearn.java
@@ -0,0 +1,17 @@
+package kr.tpc;
+
+public class Inflearn {
+    private Inflearn() {
+
+    }
+
+    /* 인스턴스 메소드 */
+    public static void tpc() {
+        System.out.println("TPC 강의 너무 재밌다");
+    }
+
+    /* 클래스 메소드 */
+    public static void java() {
+        System.out.println("Java 강의 너무 재밌다");
+    }
+}
