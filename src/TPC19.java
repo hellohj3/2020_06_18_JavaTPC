9169497393d376918f3b5d91585795ca467fd014
Myhyem <hellohj_@naver.com>
2020-06-27 12:16:25 +0900

    1. 상속

---
+++ src/TPC19.java
@@ -0,0 +1,14 @@
+import kr.tpc.Cat;
+import kr.tpc.Dog;
+
+public class TPC19 {
+    public static void main(String[] args) {
+        /* 상속 */
+        Dog d = new Dog();
+        d.eat();
+
+        Cat c = new Cat();
+        c.eat();
+
+    }
+}
