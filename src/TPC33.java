c22e0b761924b787b91f019198ce22ddd9ebeff9
Myhyem <hellohj_@naver.com>
2020-07-12 20:21:33 +0900

    1. 패키지

---
+++ src/TPC33.java
@@ -0,0 +1,8 @@
+import kr.inflean.MyClass;
+
+public class TPC33 {
+    public static void main(String[] args) {
+        MyClass m = new MyClass();
+        System.out.println("합은 : "+m.sum(3,10));
+    }
+}
---
+++ src/kr/inflean/MyClass.java
@@ -0,0 +1,13 @@
+package kr.inflean;
+
+public class MyClass {
+    public int sum(int a, int b) {
+        int hap = 0;
+
+        for(int i=a; i<=b; i++) {
+            hap+=i;
+        }
+
+        return hap;
+    }
+}
